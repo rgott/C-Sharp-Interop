ca2aac64da519d03ffbdda560d95026fa56d6f4f
Robert Gott <rgott2@students.towson.edu>
2018-03-03 13:54:18 -0500

    Initial Commit
    Basic printing functionality with C# -> C# as a baseline

---
+++ RGott.CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RGott.CSharp
+{
+    public class Program
+    {
+        /// <summary>
+        /// Prints Message
+        /// </summary>
+        /// <param name="message">message to print</param>
+        /// <returns>Language Name</returns>
+        public string Run(string message)
+        {
+            Console.WriteLine($"{ message } -From C#");
+            return "C#";
+        }
+    }
+}
---
+++ RGott/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RGott
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Hello World";
+            RGott.Interop.Language.StaticBinding(msg);
+        }
+    }
+}
