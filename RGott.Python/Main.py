b8fd98472a63c6fe3581e3e913b99eed14372893
Robert Gott <rgott2@students.towson.edu>
2018-05-21 19:49:41 -0400

    Added Python support

---
+++ RGott.Python/Main.py
@@ -0,0 +1,7 @@
+﻿import sys
+
+def Add(x, y):
+    return x + y
+
+def Run(message):
+	print(message + " -From Python")
---
+++ RGott.Python/Wrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Scripting.Hosting;
+using IPY = IronPython.Hosting;
+
+namespace RGott.Python
+{
+    public class Wrapper
+    {
+        public static dynamic GetPythonRunTime()
+        {
+            return IPY.Python.CreateRuntime().UseFile("Main.py");
+        }
+    }
+}
